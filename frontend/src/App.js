9d416fa78e4d6508333ead0ee48c57885ba72507
Shiv13527 <yuvrajpundir7788@gmail.com>
2023-03-05 00:42:35 +0530

    created chatpage, homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 
+import { Route } from 'react-router-dom';
 import './App.css';
+import  ChatPage  from './Pages/ChatPage';
+import { HomePage } from './Pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      yupp!
+      <Route path="/" component={HomePage} exact />
+      <Route path="/chats" component={ChatPage}  />
     </div>
   );
 }
