b6fb377767c1012f97ae14ae447839d201265ff1
Shiv13527 <yuvrajpundir7788@gmail.com>
2023-03-04 21:38:17 +0530

    added chat user data

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express()
@@ -6,6 +7,11 @@ app.get("/",(req,res) => {
     res.send("API is running");
 })
 
-app.get
+app.get("/",(req,res) =>{
+    response.send("API is running");
+} );
+
+// app.get();
+
 app.listen(5000,console.log("Server started on port 5000"));
 
