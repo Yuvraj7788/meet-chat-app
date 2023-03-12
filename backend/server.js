4d62cbdb72af3d8067b51ae0c7662469c0777850
Shiv13527 <yuvrajpundir7788@gmail.com>
2023-03-12 14:14:15 +0530

    setup userRoutes and userControllers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
@@ -12,14 +13,7 @@ app.get("/",(req,res) => {
     res.send("API is running successfully bro");
 });
 
-app.get('/api/chat', (req,res) => {
-    res.send(chats);
-});
-
-app.get("/api/chat/:id",(req,res) => {
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-})
+app.use('/api/user',userRoutes);
 
 const PORT = process.env.PORT || 5000
 
